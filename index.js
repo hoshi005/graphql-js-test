0ed126cc15aa93080b271a45faff86217e5f92e7
hoshi005 <hoshi005@me.com>
2022-10-30 19:46:01 +0900

    feat: タグデータのリレーションを実装

--- index.js
+++ index.js
@@ -104,12 +104,20 @@ const resolvers = {
         url: parent => `http://photos.hoshi005.net/img/${parent.id}.jpg`,
         postedBy: parent => {
             return users.find(u => u.githubLogin === parent.githubUser)
-        }
+        },
+        taggedUsers: parent => tags
+            .filter(tag => tag.photoID === parent.id)
+            .map(tag => tag.userID)
+            .map(userID => users.find(u => u.githubLogin === userID))
     },
     User: {
         postedPhotos: parent => {
             return photos.filter(p => p.githubUser === parent.githubLogin)
-        }
+        },
+        inPhotos: parent => tags
+            .filter(tag => tag.userID === parent.id)
+            .map(tag => tag.photoID)
+            .map(photoID => photos.find(p => p.id === photoID))
     }
 }
 
