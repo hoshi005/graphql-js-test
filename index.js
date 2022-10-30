fe6fc097783ab2d8dbca49b3e75c6ee69f200a0a
hoshi005 <hoshi005@me.com>
2022-10-30 19:45:46 +0900

    feat: タグのサンプルデータ

--- index.js
+++ index.js
@@ -77,6 +77,14 @@ var photos = [
     }
 ]
 
+// タグデータ.
+var tags = [
+    { "photoID": "1", "userID": "gPlake" },
+    { "photoID": "2", "userID": "sSchmidt" },
+    { "photoID": "2", "userID": "mHattrup" },
+    { "photoID": "2", "userID": "gPlake" }
+]
+
 const resolvers = {
     Query: {
         totalPhotos: () => photos.length,
